4e2dcae9723487ede3e395d7584523f4f55472ef
Roque Chacon Corrales <roke12chacon@gmail.com>
2018-03-09 13:30:44 -0600

    Parte escrita

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('roles', 'RoleController');
+Route::get('/roles/{id}/delete', 'RoleController@delete')->name('roles.delete');
 
